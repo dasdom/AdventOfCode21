7507da4ad2f5986ab13a3ace0e0c115d02d8b51b
Dominik Hauser <dom@dasdom.de>
2021-12-01 17:32:23 +0100

    Initial Commit

---
+++ AdventOfCode21/AdventOfCode21App.swift
@@ -0,0 +1,14 @@
+//  Created by Dominik Hauser on 01.12.21.
+//  
+//
+
+import SwiftUI
+
+@main
+struct AdventOfCode21App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
